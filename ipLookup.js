bba17017ad55d4f8f1fd23beaeb57edfc58cf654
Dr.NooB <itsdrnoob@gmail.com>
2023-03-17 16:21:20 +0530

    feat: implement rate limit

--- ipLookup.js
+++ ipLookup.js
@@ -2,8 +2,15 @@ const express = require('express')
 const router = express.Router()
 const requestIP = require('request-ip')
 const { IPinfoWrapper, ApiLimitError } = require("node-ipinfo")
+const rateLimit = require('express-rate-limit')
 
-router.get('/', function(req, res) {
+const apiLimit = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 25,
+    message: "Too many requests, please try again later."
+})
+
+router.get('/', apiLimit, function(req, res) {
     var token = req.query.token
     if (token == process.env.TOKEN) {
         const ipinfo = new IPinfoWrapper(process.env.IP_TOKEN)
